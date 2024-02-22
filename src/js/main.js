e029cc33defc8b0fd10d516e4d4db05a08a1b178
Oronia <abdullahhb1314.3@gmail.com>
2024-02-22 13:47:21 +0530

    added scroller

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 
-
+let content = document.getElementById("content");
 
 let nav = document.getElementById("nav");
 let navCode = `
             <div class="navItem">Home</div>
             <div class="navItem" style="box-shadow: none;">Essential Oil</div>
 `;
-
 function toggleNav()
 {
     if(nav.innerHTML == "")
@@ -17,4 +16,70 @@ function toggleNav()
     {
         nav.innerHTML = "";
     }
+}
+
+
+
+/* ----- ARROW ----- */
+
+let arrow = document.getElementById("arrowNav");
+let scrolling = false;
+
+function toggleArrow(scroll)
+{
+    if(scrolling) return;
+    
+    if(scroll > 250)
+    {
+        arrow.classList.remove("hidden");
+    }
+    else
+    {
+        arrow.classList.add("hidden");
+    }
+}
+
+window.addEventListener("scroll", (event) => {
+    toggleArrow(this.scrollY);
+});
+
+let timer;
+
+function scrollToTop()
+{
+    scroll();
+    
+    timer = setInterval(scrollUp, 1);
+    
+    content.addEventListener("touchstart", function () {
+        stopScroll();
+        content.removeEventListener('touchstart', null);
+    });
+}
+
+function scroll()
+{
+    scrolling = true;
+    arrow.classList.add("hidden");
+    content.style.filter = "blur(1.5px) opacity(0.9)";
+}
+
+function scrollUp()
+{
+    if(this.scrollY > 0)
+    {
+        scrollTo(0, this.scrollY - 10);
+    }
+    else
+    {
+        stopScroll();
+    }
+}
+
+function stopScroll()
+{
+    scrolling = false;
+    content.style.filter = "none";
+    // content.classList.remove("blur");
+    clearTimeout(timer);
 }
